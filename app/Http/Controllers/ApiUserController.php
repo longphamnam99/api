6a8a1efccff6a45c3005cf6e52c8a03a6682026d
longphamnam99 <longkul1478@gmail.com>
2023-03-04 13:45:31 +0700

    init

---
+++ app/Http/Controllers/ApiUserController.php
@@ -0,0 +1,101 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\ApiRegisterRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\ApiLoginRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+
+class ApiUserController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth:api', ['except' => ['login', 'register']]);
+    }
+
+    public function register(ApiRegisterRequest $request)
+    {
+        $user = new User();
+        $user->fill($request->all());
+        $user->password = Hash::make($request->password);
+        $saved = $user->save();
+
+        if ($saved) {
+            $res = [
+                'status' => 200,
+                'message' => 'ok',
+            ];
+            return response()->json($res);
+        }
+
+        $res = [
+            'status' => 400,
+            'message' => 'not ok',
+        ];
+        return response()->json($res);
+    }
+
+    public function refresh()
+    {
+        return $this->create_new_token(auth()->refresh());
+    }
+
+    protected function create_new_token($token)
+    {
+        return [
+            'access_token' => $token,
+            'token_type' => 'bearer',
+            'expires_in' => auth('api')->factory()->getTTL() * 60,
+            'user' => auth()->user()
+        ];
+    }
+
+    public function login(ApiLoginRequest $request)
+    {
+        if (!$token = Auth::attempt(['email' => $request->email, 'password' => $request->password,])) {
+            $res = [
+                'status' => 400,
+                'message' => 'Incorrect account or password.',
+            ];
+            return response()->json($res);
+        }
+
+        $res = [
+            'status' => 200,
+            'message' => 'ok',
+            'data' => [
+                'token' => $this->create_new_token($token)['access_token']
+            ]
+        ];
+        return response()->json($res);
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        $res = [
+            'status' => 200,
+            'message' => 'ok'
+        ];
+        return response()->json($res);
+    }
+
+    public function info(request $request)
+    {
+        $user =  Auth::user();
+        $data = [
+            "id" => $user["id"],
+            "name" => $user["name"]
+        ];
+        $res = [
+            'status' => 200,
+            'message' => 'ok',
+            'data' => $data
+        ];
+        return response()->json($res);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+
+Route::post('/user/register', 'App\Http\Controllers\ApiUserController@register');
+Route::post('/user/login', 'App\Http\Controllers\ApiUserController@login');
+Route::get('/user/info', 'App\Http\Controllers\ApiUserController@info');
+Route::get('/user/refresh', 'App\Http\Controllers\ApiUserController@refresh');
+
+// Route::group([
+//     'middleware' => 'api',
+//     'prefix' => 'auth'
+// ], function ($router) {
+//     Route::post('/user/login', [ApiUserController::class, 'login']);
+//     Route::post('/user/register', [ApiUserController::class, 'register']);
+//     Route::post('/user/logout', [ApiUserController::class, 'logout']);
+//     Route::post('/user/info', [ApiUserController::class, 'info']);    
+// });
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
